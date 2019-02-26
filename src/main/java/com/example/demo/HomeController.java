f1e6ba76eeacdd0f7c0a4927abe6c955393dd0aa
jihyunle <jennifer.jlee@outlook.com>
2019-02-26 17:14:29 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    ToDoRepository toDoRepository;
+
+    @RequestMapping("/")
+    public String listToDos(Model model){
+        model.addAttribute("toDos", toDoRepository.findAll());
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String todoForm(Model model){
+        model.addAttribute("toDo", new ToDo());
+        return "todoform";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid ToDo toDo, BindingResult result){
+        if (result.hasErrors()){
+            return "todoform";
+        }
+        toDoRepository.save(toDo);
+        return "redirect:/";
+    }
+}
+
---
+++ src/main/java/com/example/demo/ToDo.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/*Remember the Entity, Id, GeneratedValue tags*/
+
+@Entity
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 3)
+    private String taskName;
+
+    @NotNull
+    @Size(min = 3)
+    private String dueDate;
+
+    @NotNull
+    @Size(min = 1)
+    private String priority;
+
+    public ToDo(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
